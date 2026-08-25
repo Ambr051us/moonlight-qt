2078adb943c10ffc823cb5610eebeacf6bdf95d3
Ambrus Sümegi <ambrus95@gmail.com>
2026-08-25 13:07:08 +0200

    build: support standalone Visual Studio Build Tools

--- scripts/generate-bundle.bat
+++ scripts/generate-bundle.bat
@@ -67,7 +67,7 @@ mkdir %INSTALLER_FOLDER%
 rem Find Visual Studio and run vcvarsall.bat
 call "%SOURCE_ROOT%\scripts\find-vswhere.bat"
 if !ERRORLEVEL! NEQ 0 goto Error
-for /f "usebackq delims=" %%i in (`%VSWHERE% -latest -property installationPath`) do (
+for /f "usebackq delims=" %%i in (`%VSWHERE% -latest -products * -property installationPath`) do (
     call "%%i\VC\Auxiliary\Build\vcvarsall.bat" x86
 )
 if !ERRORLEVEL! NEQ 0 goto Error
